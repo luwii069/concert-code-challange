8522b16d59c407ed0e726c438bb2507401572037
louis mulaa <louismulaa06@gmail.com>
2024-06-11 00:31:56 +0300

    concert code challange

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,32 +1,90 @@
 class Band:
     def __init__(self, name, hometown):
         self.name = name
-        self.hometown = hometown
+        if not isinstance(hometown, str):
+            raise ValueError("Hometown must be a string.")
+        self._hometown = hometown
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+        else:
+            raise ValueError("Name must be a non-empty string.")
+
+    @property
+    def hometown(self):
+        return self._hometown
+
+    @hometown.setter
+    def hometown(self, value):
+        # Make hometown immutable
+        raise Exception("Hometown attribute is immutable.")
 
     def concerts(self):
-        pass
+        return [concert for concert in Concert.all if concert.band == self]
 
     def venues(self):
-        pass
+        return list(set([concert.venue for concert in self.concerts()]))
 
     def play_in_venue(self, venue, date):
-        pass
+        return Concert(date, self, venue)
 
     def all_introductions(self):
-        pass
+        return [concert.introduction() for concert in self.concerts() if concert.introduction()]
 
 
 class Concert:
+    all = []
+
     def __init__(self, date, band, venue):
         self.date = date
         self.band = band
         self.venue = venue
+        Concert.all.append(self)
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._date = value
+        else:
+            raise ValueError("Date must be a non-empty string.")
+
+    @property
+    def band(self):
+        return self._band
+
+    @band.setter
+    def band(self, value):
+        if isinstance(value, Band):
+            self._band = value
+        else:
+            raise ValueError("Band must be an instance of Band class.")
+
+    @property
+    def venue(self):
+        return self._venue
+
+    @venue.setter
+    def venue(self, value):
+        if isinstance(value, Venue):
+            self._venue = value
+        else:
+            raise ValueError("Venue must be an instance of Venue class.")
 
     def hometown_show(self):
-        pass
+        return self.band.hometown == self.venue.city
 
     def introduction(self):
-        pass
+        return f"Hello {self.venue.city}!!!!! We are {self.band.name} and we're from {self.band.hometown}"
 
 
 class Venue:
@@ -34,8 +92,36 @@ class Venue:
         self.name = name
         self.city = city
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+        else:
+            raise ValueError("Name must be a non-empty string.")
+
+    @property
+    def city(self):
+        return self._city
+
+    @city.setter
+    def city(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._city = value
+        else:
+            raise ValueError("City must be a non-empty string.")
+
     def concerts(self):
-        pass
+        return [concert for concert in Concert.all if concert.venue == self]
 
     def bands(self):
-        pass
+        return list({concert.band for concert in self.concerts()})
+
+    def concert_on(self, date):
+        for concert in self.concerts():
+            if concert.date == date:
+                return concert
+        return None
--- lib/debug.py
+++ lib/debug.py
@@ -5,6 +5,39 @@ from classes.many_to_many import Band
 from classes.many_to_many import Concert
 from classes.many_to_many import Venue
 
+
+# Create instances of Band
+band1 = Band("The Beatles", "Liverpool")
+band2 = Band("Led Zeppelin", "London")
+
+# Create instances of Venue
+venue1 = Venue("Madison Square Garden", "New York City")
+venue2 = Venue("Wembley Stadium", "London")
+
+# Create instances of Concert
+concert1 = Concert("2024-07-15", band1, venue1)
+concert2 = Concert("2024-08-20", band2, venue2)
+
+# Test methods of Band class
+print("Band Name:", band1.name)  # Output: The Beatles
+print("Band Hometown:", band1.hometown)  # Output: Liverpool
+print("Concerts by Band:", band1.concerts())  # Output: [concert1]
+print("Venues for Band:", band1.venues())  # Output: [venue1]
+print("Introduction for Band:", band1.all_introductions())  # Output: ["Hello New York City!!!!! We are The Beatles and we're from Liverpool"]
+
+# Test methods of Venue class
+print("Venue Name:", venue1.name)  # Output: Madison Square Garden
+print("Venue City:", venue1.city)  # Output: New York City
+print("Concerts at Venue:", venue1.concerts())  # Output: [concert1]
+print("Bands at Venue:", venue1.bands())  # Output: [band1]
+
+# Test methods of Concert class
+print("Concert Date:", concert1.date)  # Output: 2024-07-15
+print("Concert Band:", concert1.band)  # Output: Band object for The Beatles
+print("Concert Venue:", concert1.venue)  # Output: Venue object for Madison Square Garden
+print("Hometown Show:", concert1.hometown_show())  # Output: True
+print("Introduction:", concert1.introduction())  # Output: "Hello New York City!!!!! We are The Beatles and we're from Liverpool"
+
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
